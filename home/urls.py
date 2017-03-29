037a48bfd323e73f4b455863abd972369dbed5d2
akshayreddyb <akshayreddybeeram@gmail.com>
2017-03-30 03:02:07 +0530

    made changes in urls

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,10 @@ app_name = "home"
 
 urlpatterns = [
     url(r'^login$',views.login,name='login'),
-    url(r'^success/',views.success,name='success'),
+    #url(r'^success/',views.success,name='success'),
     url(r'^register$', views.register, name='register'),
     url(r'^forgot_pwd$', views.forgot, name='forgot'),
     url(r'^validateUser/', views.validateUser),
-    url(r'^$', views.home, name='home'),
+    url(r'^home$', views.home, name='home'),
     # url(r'^$', views.login, name='login'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Administrator
-
+validation=False
+def direct(request):
+	return redirect('/login')	
 def login(request):
      return render(request, 'home/login_page.html', {'failed_login': False})
-
-def success(request):
-     return render(request, 'home/success.html')
 
 def register(request):
 	return render(request, 'home/register.html')
@@ -14,18 +13,30 @@ def forgot(request):
 	return render(request, 'home/forgot_pwd.html')
 
 def home(request):
-	return render(request, 'home/home.html')
+	global validation
+	if(validation==True):
+	   return render(request, 'home/home.html')
+	else:
+		return redirect('/login')
 
 def validateUser(request):
 	name = request.POST['uname']
 	pwd = request.POST['pwd']
 	usr = None
+	global validation
 	try:
 		usr = Administrator.objects.get(username=name)
 	except Administrator.DoesNotExist:
 		pass
 
 	if usr is not None and usr.password == pwd:
+		validation=True
 		return redirect('/home')
 	else:
 		return render(request, 'home/login_page.html', {'failed_login': True})
+
+
+def logout(request):
+	global validation
+	validation=False
+	redirect('/login')
